d7cf9667f7f69ee4c60de598c34e72ecff8b2c1d
Thirumalaa Srinivas <thrmlsrnvs@gmail.com>
2016-10-03 14:00:46 -0500

    Embed script and route added; with payload script placeholder.

---
+++ public/javascripts/widgets/wthr.js
@@ -0,0 +1,1 @@
+console.log('wthr widget is here!');
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,29 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Weather Widget' });
 });
 
+
+router.get('/widgets/:name', function(req, res, next) {
+
+  var options = {
+    root: __dirname + '/../public/javascripts/widgets/',
+    headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+    }
+  };
+
+  var fileName = req.params.name+'.js';
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      console.log(err);
+      res.status(err.status).end();
+    }
+    else {
+      console.log('Sent:', fileName);
+    }
+  });
+
+});
+
+
 module.exports = router;
